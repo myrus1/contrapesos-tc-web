938e075fb8f22a6d72a92048ce9a893fe2fa4bba
myrus1 <mendezfacundo80@gmail.com>
2026-03-10 16:50:47 -0300

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://myrus1.github.io',
+  base: '/contrapesos-tc-web',
+});
